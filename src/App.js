8149ddcb2e3ea5ab5631bfe5be9f728174a5b87c
TatyCat <williams.tatyanna@gmail.com>
2019-05-16 20:17:44 -0400

    Hardcoded UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+
+    return (
+      <>
+        <section>
+          <h1>3UP</h1>
+          <h2>3 Accomplishments of
+          <span> Tuesday, May 16, 2019</span>
+          </h2>
+        </section>
+
+        <article className="page">
+          <p>Go to the bank and desposit found money</p>
+        </article>
+        <br />
+        <article className="page">
+          <p>Push a project to github and create an app</p>
+        </article>
+
+        <br />
+
+        <article className="page">
+          <p>Look up interview questions for sourcetoad interview </p>
+        </article>
+      </>
+    )
   }
 }
 
