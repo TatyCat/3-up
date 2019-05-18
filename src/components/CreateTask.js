ef4bc938d93d57979a06dd4a09b126ff8ef6d7aa
TatyCat <williams.tatyanna@gmail.com>
2019-05-18 09:06:26 -0400

    spacing added to textarea

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 class CreateTask extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { task: '' };
 
     this.handleFormChange = this.handleFormChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleFormChange = (e) => {
+    console.log(e.target.value)
     this.setState({
       task: e.target.value,
     })
@@ -26,7 +27,6 @@ class CreateTask extends Component {
           <label>
             New Task :
             <textarea rows="3" cols="50" value={this.state.value} onChange={this.handleFormChange} placeholder="Today I need to..." />
-
           </label>
           <button><i className="fas fa-check"></i></button>
         </form>
