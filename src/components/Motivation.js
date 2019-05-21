6bce3e3da090f7415d6238273461e33b65db6185
TatyCat <williams.tatyanna@gmail.com>
2019-05-21 19:49:03 -0400

    Motivation array updated with more.

--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class Motivation extends Component {
   state = {
-    motivation: ["Strength", "Joy", "Wealth", "Health"]
+    motivation: [
+      "Strength", "Joy", "Wealth", "Health", "Competence", "Clean", "Hospitality", "Attraction", "Intuition", "Tranquility", "Integrity", "Focus", "Aspiration", "Industriousness", "Love", "Beauty", "Peace", "Grace", "Worth", "Power", "Value", "Truth", "Light", "Luck", "Intelligence", "Abundance", "Authenticity", "Awesomeness", "Blessing", "Confidence", "Honesty", "Empathy", "Enough", "Forgiveness", "Gratitude", "Happiness", "Success", "Innovation", "Mindfulness", "Memorable", "Open(ness)", "Perfection", "Prosperity", "Radience", "Self-Love", "Fearless", "Courage", "Rest and Relaxation", "Healing", "Respect", "Determination", "Complete", "Power", "Uncomparable",
+    ]
   }
 
 
